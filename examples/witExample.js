5357686b071fd82a3326576afd7aa61c46862f32
Jonnatan Oyarzun <jonsxaero@gmail.com>
2017-07-30 10:09:40 -0400

    Update first Wit example to do it more simple

--- examples/witExample.js
+++ examples/witExample.js
@@ -1,16 +1,11 @@
 const speeech = require("../src/index");
 const get = require("lodash.get");
-const say = require("say");
 
 const serviceConfig = require("../witkeyfile.json");
 
 const process = result => {
   const intent = get(result, "entities.intent[0].value");
-  if (intent === "weather") {
-    const location = get(result, "entities.location[0].value");
-    say.speak("You want weather of " + location);
-  }
-  console.log(result.entities);
+  console.log(`The  intent is ${intent}`);
 };
 
 speeech.emit("start", speeech.witService(serviceConfig));
