8cedf33a474f53ec9eaa9e84e3cbbc08069b9a03
Jonnatan Oyarzun <jonsxaero@gmail.com>
2017-08-21 00:44:59 -0300

    Add test for detector

--- test/detector.test.js
+++ test/detector.test.js
@@ -1,14 +1,34 @@
-const detector = require("../src/detector");
+describe("Detector", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
 
-describe("When detector emit hotword", () => {
-  it("should emit an hotword event in speeech", () => {
-    const mockSpeeech = {
-      emit: jest.fn()
-    };
+  describe("When create detector and emit hotword", () => {
+    it("should emit an hotword event in speeech", () => {
+      const detector = require("../src/detector");
+      const mockSpeeech = {
+        emit: jest.fn()
+      };
 
-    const mydetector = detector(mockSpeeech);
-    mydetector.emit("hotword");
+      const mydetector = detector(mockSpeeech);
+      mydetector.emit("hotword");
 
-    expect(mockSpeeech.emit.mock.calls.length).toBe(1);
+      expect(mockSpeeech.emit.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe("When create two detector and emit hotword", () => {
+    it("should emit just one hotword event in speeech", () => {
+      const detector = require("../src/detector");
+      const mockSpeeech = {
+        emit: jest.fn()
+      };
+
+      let mydetector = detector(mockSpeeech);
+      mydetector = detector(mockSpeeech);
+      mydetector.emit("hotword");
+
+      expect(mockSpeeech.emit.mock.calls.length).toBe(1);
+    });
   });
 });
